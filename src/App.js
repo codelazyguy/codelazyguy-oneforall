6f57f11c5df390ef7f91ec7760193cfbe824f352
codelazyguy <codelazyguy@gmail.com>
2021-01-01 13:01:04 +0700

    HappyNewYear

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="App-welcome">
           Welcome Code Lazy Guy - This is OfA project
         </p>
         <a
