f7ceac8ca9de321f5acc40dabab54c777cccca3a
codelazyguy <codelazyguy@gmail.com>
2021-01-02 22:56:13 +0700

    codelazyguy-oneforall-2021

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Here
         </a>
         <p>Happy New Year</p>
       </header>
